65441e96e14cd6e353ff420ac977f70538c2769e
Shristi Koju <kojushristi4@gmail.com>
2024-08-06 10:20:14 +0545

    Did some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./css/*.css", "./*html"],
+  content: ["./css/*.css", "./*.html"],
   theme: {
     extend: {
       fontFamily: {
-        young: ["Young Serif", "serif"],
-        outfit: ["Outfit","sans-serif"],
+        poppins: ['Poppins', 'sans-serif'],
+        young: ['Young Serif', 'serif'],
+        outfit: ['Outfit', 'sans-serif'],
       },
       colors: {
-       'rose-light': 'hsl(330, 100%, 98%)',
-       'color-brown': 'hsl(14, 45%, 36%)',
+        'color-rose-dark': 'hsl(332, 51%, 32%)', //hsl(332, 51%, 32%)
+        'color-rose-light': 'hsl(330, 100%, 98%)',
+        'color-brown': 'hsl(14, 45%, 36%)',
+        'color-stone-dark': 'hsl(30, 10%, 34%)',
+        'color-stone': 'hsl(30, 10%, 34%)',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            'ul > li::marker': {
+              'font-weight': '600',
+              'color': '#757575', // Replace with your desired color
+            },
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
-
